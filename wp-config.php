b5b026d9185eb325ccad54f468b9d254002e4f85
mchandleraz <matt@mchandler.me>
2013-02-14 09:42:44 -0700

    removed db info

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'dnaprss');
+define('DB_NAME', '');
 
 /** MySQL database username */
-define('DB_USER', 'dnaprss');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Ytxw45W7cFj6BXsf');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
